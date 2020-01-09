8febad9bd6c1ffdb4435b2590c2cbf5793bd0274
Urgen Sherpa <urgen.nyc@gmail.com>
2020-01-09 19:32:06 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
   if board[0] == " "  || board[0] == "" || board[0] == nil
     false
-  elsif board[0] == "X" || board[0] == "O"
+  else board[0] == "X" || board[0] == "O"
     true
   end
 end
